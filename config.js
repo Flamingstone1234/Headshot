c2bd96653c5eb5dfe5bd3c70ee6c275d0eae16b4
Flamingstone1234 <Tamangalex2013@gmail.com>
2026-05-02 17:12:26 -0700

    Update username to dougie and password to 1234

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
   challenge: true, // Set to true if you want to enable password protection.
-  password: "test123",
+  password: "1234",
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    dougie: "1234",
   },
 };
 
